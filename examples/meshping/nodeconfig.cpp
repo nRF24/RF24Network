7cfee69db18e7ce756e742d796032d312528f86f
maniacbug <maniacbug@ymail.com>
2011-06-10 21:12:31 -0700

    Added node configuration capability

---
+++ examples/meshping/nodeconfig.cpp
@@ -0,0 +1,55 @@
+#include <WProgram.h>
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+
+// Avoid spurious warnings
+#undef PROGMEM 
+#define PROGMEM __attribute__(( section(".progmem.data") )) 
+#undef PSTR 
+#define PSTR(s) (__extension__({static prog_char __c[] PROGMEM = (s); &__c[0];}))
+
+// Where in EEPROM is the address stored?
+uint8_t* address_at_eeprom_location = (uint8_t*)10;
+
+// What flag value is stored there so we know the value is valid?
+const uint8_t valid_eeprom_flag = 0xdf;
+
+uint8_t nodeconfig_read(void)
+{
+  uint8_t result = 0;
+
+  // Look for the token in EEPROM to indicate the following value is
+  // a validly set node address 
+  if ( eeprom_read_byte(address_at_eeprom_location) == valid_eeprom_flag )
+  {
+    // Read the address from EEPROM
+    result = eeprom_read_byte(address_at_eeprom_location+1);
+  }
+
+  printf_P(PSTR("ADDRESS: %u\n\r"),result);
+  
+  return result;
+}
+
+void nodeconfig_listen(void)
+{
+  //
+  // Listen for serial input, which is how we set the address
+  //
+  if (Serial.available())
+  {
+    // If the character on serial input is in a valid range...
+    char c = Serial.read();
+    if ( c >= '0' && c <= '5' )
+    {
+      // It is our address
+      eeprom_write_byte(address_at_eeprom_location,valid_eeprom_flag);
+      eeprom_write_byte(address_at_eeprom_location+1,c-'0');
+
+      // And we are done right now (no easy way to soft reset)
+      printf_P(PSTR("\n\rManually reset address to: %c\n\rPress RESET to continue!"),c);
+      while(1);
+    }
+  }
+}
+// vim:ai:cin:sts=2 sw=2 ft=cpp
---
+++ examples/meshping/nodeconfig.h
@@ -0,0 +1,7 @@
+#ifndef __NODECONFIG_H__
+#define __NODECONFIG_H__
+
+uint8_t nodeconfig_read(void);
+void nodeconfig_listen(void);
+
+#endif // __NODECONFIG_H__
