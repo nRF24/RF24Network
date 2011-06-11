8c5aa77afb4acdc144355bcd9069292f82e6700e
maniacbug <maniacbug@ymail.com>
2011-06-10 21:20:20 -0700

    Added copyright header

--- examples/meshping/nodeconfig.cpp
+++ examples/meshping/nodeconfig.cpp
@@ -1,3 +1,11 @@
+/*
+ Copyright (C) 2011 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
 #include <WProgram.h>
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
--- examples/meshping/nodeconfig.h
+++ examples/meshping/nodeconfig.h
@@ -1,3 +1,11 @@
+/*
+ Copyright (C) 2011 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
 #ifndef __NODECONFIG_H__
 #define __NODECONFIG_H__
 
