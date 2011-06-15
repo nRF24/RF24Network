7b64c51b7046af58db0ee558078942f631c992c9
maniacbug <maniacbug@ymail.com>
2011-06-14 21:43:35 -0700

    Converte header-to-string to print proper octal addresses

--- RF24Network.cpp
+++ RF24Network.cpp
@@ -295,7 +295,7 @@ bool RF24Network::write_to_pipe( uint16_t node, uint8_t pipe )
 const char* RF24NetworkHeader::toString(void) const
 {
   static char buffer[45];
-  snprintf(buffer,sizeof(buffer),"id %04x from %04x to %04x type %c",id,from_node,to_node,type);
+  snprintf(buffer,sizeof(buffer),"id %04x from 0%o to 0%o type %c",id,from_node,to_node,type);
   return buffer;
 }
 
