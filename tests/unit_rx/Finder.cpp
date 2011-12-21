6416d82009ad160297a8b1c7aae862a9056a94fe
maniacbug <maniacbug@ymail.com>
2011-12-21 10:40:26 -0800

    Stub finder class.  Will take over finder message duties

---
+++ tests/unit_rx/Finder.cpp
@@ -0,0 +1,19 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+// STL headers
+// C headers
+// Framework headers
+// Library headers
+// Project headers
+// This component's header
+#include <Finder.h>
+
+/****************************************************************************/
+
+// vim:cin:ai:sts=2 sw=2 ft=cpp
---
+++ tests/unit_rx/Finder.h
@@ -0,0 +1,30 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#ifndef __FINDER_H__
+#define __FINDER_H__
+
+// STL headers
+// C headers
+// Framework headers
+// Library headers
+// Project headers
+
+/**
+ * Example for how classes should be declared
+ */
+
+class Finder
+{
+private:
+protected:
+public:
+};
+
+#endif // __FINDER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
