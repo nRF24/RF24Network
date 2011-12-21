37e55d496d5fc6a331b6ea1b79cca7fd95bf1b81
maniacbug <maniacbug@ymail.com>
2011-12-21 14:44:02 -0800

    Moved network startup into main(), so it only fires up the network ONCE.  Compiles only, not tested.

--- tests/unit/runner.cpp
+++ tests/unit/runner.cpp
@@ -12,20 +12,32 @@
 #include <cxxtest/RealDescriptions.h>
 #include <cxxtest/TestMain.h>
 #include <cxxtest/ErrorPrinter.h>
+#include <cxxtest/Root.cpp>
+
+#include <RF24Network_config.h>
+#include <RF24Network.h>
+#include <RF24.h>
 
 extern void board_start(const char*);
+extern RF24Network network;
+extern RF24 radio;
 
-int main( void ) {
-    CxxTest::ErrorPrinter tmp;
+int main( void ) 
+{
+  CxxTest::ErrorPrinter tmp;
 
+  while(1)
+  {
     board_start(__FILE__);
+    
+    SPI.begin();
+    radio.begin();
+    network.begin(/* channel */100,/* this node */0);
 
     CxxTest::Main<CxxTest::ErrorPrinter>( tmp, 0, NULL );
 
-    printf("Tests complete.  Stopping\r\n");
-	while(1)
-	{
-	}
+    printf("Tests complete.  Restarting...\r\n");
+  }
 }
-#include <cxxtest/Root.cpp>
 const char* CxxTest::RealWorldDescription::_worldName = "cxxtest";
+// vim:cin:ai:sts=2 sw=2 ft=cpp
