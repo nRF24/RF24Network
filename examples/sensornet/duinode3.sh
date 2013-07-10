887dc6f1d8bc045247cc98baa5aa31abcb56c253
maniacbug <maniacbug@ymail.com>
2013-07-10 16:18:42 -0700

    Tweaks for current build system

--- examples/sensornet/duinode3.sh
+++ examples/sensornet/duinode3.sh
@@ -1,1 +1,3 @@
-jam F_CPU=8000000 "CCFLAGS=-include DuinodeV3.h" -dx UPLOAD_SPEED=57600 u0 && screen /dev/ttyUSB0 57600
+AVR_BIN=/usr/local/avr/bin AVR_ETC=/usr/local/avr/etc jam F_CPU=8000000 sensornet.elf
+touch sensornet.pde
+AVR_BIN=/usr/local/avr/bin AVR_ETC=/usr/local/avr/etc jam F_CPU=8000000 "CCFLAGS=-include DuinodeV3.h" -dx UPLOAD_SPEED=57600 u1 && screen /dev/ttyUSB1 57600
