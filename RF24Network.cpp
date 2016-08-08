deeb0b2284b74ef012664fd5cc12ecb923846d9d
TMRh20 <tmrh20@gmail.com>
2016-08-07 22:36:12 -0600

    Fix: Don't use W_TX_PAYLOAD_NO_ACK flag
    Per https://github.com/TMRh20/RF24Network/issues/98 and
    https://github.com/TMRh20/RF24Mesh/issues/91 the flag seems to cause
    problems for many clone modules

--- RF24Network.cpp
+++ RF24Network.cpp
@@ -1041,7 +1041,7 @@ bool RF24Network::write_to_pipe( uint16_t node, uint8_t pipe, bool multicast )
   
   radio.openWritingPipe(out_pipe);
 
-  ok = radio.writeFast(frame_buffer, frame_size,multicast);
+  ok = radio.writeFast(frame_buffer, frame_size,0);
   
   if(!(networkFlags & FLAG_FAST_FRAG)){
     ok = radio.txStandBy(txTimeout);
