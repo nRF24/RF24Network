eae724fc0912024550bbbd36b4be2dacda237050
Chenny Langness <cnlangness@gmail.com>
2018-04-21 15:31:52 -0400

    Adding type definition of a tuple for peek.

--- RPi/pyRF24Network/pyRF24Network.cpp
+++ RPi/pyRF24Network/pyRF24Network.cpp
@@ -126,7 +126,7 @@ BOOST_PYTHON_MODULE(RF24Network){
         }
         { //::RF24Network::peek
 
-            typedef void ( *peekvoid )( ::RF24Network& , size_t) ;
+            typedef bp::tuple ( *peekvoid )( ::RF24Network& , size_t) ;
 
             RF24Network_exposer.def(
                 "peek"
