a49c2c40f426f50cc7b4f11eebcc18f7cbae0864
brendan <2bndy5@gmail.com>
2021-02-03 15:45:11 -0800

    update py examples

--- RPi/pyRF24Network/examples/helloworld_rx.py
+++ RPi/pyRF24Network/examples/helloworld_rx.py
@@ -8,40 +8,25 @@
 #
 from __future__ import print_function
 import time
-from struct import *
-from RF24 import *
-from RF24Network import *
+import struct
+from RF24 import RF24
+from RF24Network import RF24Network
 
-# CE Pin, CSN Pin, SPI Speed
-
-# Setup for GPIO 22 CE and GPIO 25 CSN with SPI Speed @ 1Mhz
-#radio = radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_18, BCM2835_SPI_SPEED_1MHZ)
-
-# Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 4Mhz
-#radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ)
-
-# Setup for GPIO 22 CE and CE1 CSN with SPI Speed @ 8Mhz
-#radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
-
-# Setup for GPIO 22 CE and CE0 CSN for RPi B+ with SPI Speed @ 8Mhz
-#radio = RF24(RPI_BPLUS_GPIO_J8_22, RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ)
-
-radio = RF24(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ)
+# generic Setup for GPIO 22 CE and GPIO 8 CSN
+radio = RF24(22, 0)
 network = RF24Network(radio)
 
-millis = lambda: int(round(time.time() * 1000))
-octlit = lambda n:int(n, 8)
-
 # Address of our node in Octal format (01, 021, etc)
-this_node = octlit("00")
+this_node = 0o0
 
 # Address of the other node
-other_node = octlit("01")
+other_node = 0o1
 
 radio.begin()
 time.sleep(0.1)
 network.begin(90, this_node)    # channel 90
-radio.printDetails()
+# radio.printDetails()
+radio.printPrettyDetails()
 packets_sent = 0
 last_sent = 0
 
@@ -50,7 +35,7 @@ while 1:
     while network.available():
         header, payload = network.read(8)
         print("payload length ", len(payload))
-        ms, number = unpack('<LL', bytes(payload))
+        ms, number = struct.unpack('<LL', bytes(payload))
         print('Received payload ', number, ' at ', ms, ' from ', oct(header.from_node))
     time.sleep(1)
 
--- RPi/pyRF24Network/examples/helloworld_tx.py
+++ RPi/pyRF24Network/examples/helloworld_tx.py
@@ -8,57 +8,39 @@
 #
 from __future__ import print_function
 import time
-from struct import *
-from RF24 import *
-from RF24Network import *
+import struct
+from RF24 import RF24
+from RF24Network import RF24Network, RF24NetworkHeader
 
-# CE Pin, CSN Pin, SPI Speed
 
-# Setup for GPIO 22 CE and GPIO 25 CSN with SPI Speed @ 1Mhz
-#radio = radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_18, BCM2835_SPI_SPEED_1MHZ)
-
-# Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 4Mhz
-#radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ)
-
-# Setup for GPIO 22 CE and CE1 CSN with SPI Speed @ 8Mhz
-#radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
-
-# Setup for GPIO 22 CE and CE0 CSN for RPi B+ with SPI Speed @ 8Mhz
-#radio = RF24(RPI_BPLUS_GPIO_J8_22, RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ)
-
-radio = RF24(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ)
+# generic Setup for GPIO 22 CE and GPIO 8 CSN
+radio = RF24(22, 0)
 network = RF24Network(radio)
 
-millis = lambda: int(round(time.time() * 1000)) & 0xffffffff
-octlit = lambda n:int(n, 8)
-
 # Address of our node in Octal format (01,021, etc)
-this_node = octlit("01")
+this_node = 0o1
 
 # Address of the other node
-other_node = octlit("00")
+other_node = 0o0
 
 #ms -  How long to wait before sending the next message
 interval = 2000
 
 radio.begin()
-time.sleep(0.1);
+time.sleep(0.1)
 network.begin(90, this_node)    # channel 90
-radio.printDetails()
+# radio.printDetails()
+radio.printPrettyDetails()
 packets_sent = 0
 last_sent = 0
 
 while 1:
     network.update()
-    now = millis()
+    now = time.monotonic_ns() / 1000
     # If it's time to send a message, send it!
-    if ( now - last_sent >= interval  ):
+    if now - last_sent >= interval:
         last_sent = now
-        print('Sending ..')
-        payload = pack('<LL', millis(), packets_sent )
+        payload = struct.pack('<LL', time.monotonic_ns() / 1000, packets_sent)
         packets_sent += 1
         ok = network.write(RF24NetworkHeader(other_node), payload)
-        if ok:
-            print('ok.')
-        else:
-            print('failed.')
+        print("Sending...", "ok." if ok else "failed.")
