d42fe5e52621b062755aff6659acc67c6ba0ff5d
Brendan <2bndy5@gmail.com>
2021-07-02 14:03:44 -0700

    add to last commit for completeness

--- RF24Network_config.h
+++ RF24Network_config.h
@@ -48,7 +48,7 @@
          * @brief The allocated size of the incoming frame buffer.
          *
          * This is the user-cache, where incoming data is stored.
-         * Data is stored using Frames: Header (8 bytes) + Message_Size (2 bytes) + Data (? bytes)
+         * Data is stored using Frames: Header (8 bytes) + Message_Size (2 bytes) + Message_Data (? bytes)
          * @note Over-The-Air (OTA) transmissions don't include the message size in the transmitted packet.
          */
         #define MAIN_BUFFER_SIZE (MAX_PAYLOAD_SIZE + FRAME_HEADER_SIZE)
