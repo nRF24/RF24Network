8d4ecc83d4a45f6214b5ac88289a43592bbef0a2
TMRh20 <tmrh20@gmail.com>
2025-02-27 04:41:00 -0600

    Missing bracket...

--- RF24Network.cpp
+++ RF24Network.cpp
@@ -722,7 +722,7 @@ bool ESBNetwork<radio_t>::main_write(RF24NetworkHeader& header, const void* mess
 
 #if defined(DISABLE_FRAGMENTATION)
 
-    frame_size = rf24_min(len + sizeof(RF24NetworkHeader), max_frame_payload_size + sizeof(RF24NetworkHeader);
+    frame_size = rf24_min(len + sizeof(RF24NetworkHeader), max_frame_payload_size + sizeof(RF24NetworkHeader));
     return _write(header, message, rf24_min(len, max_frame_payload_size), writeDirect);
 
 #else // !defined(DISABLE_FRAGMENTATION)
