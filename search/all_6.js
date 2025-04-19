8adc086a352d58b182fda6933c40aec415b4fd69
2bndy5 <2bndy5@users.noreply.github.com>
2025-04-19 08:09:15 +0000

    deploy: 380efb83c9ee8d0455279926fd9a6944dff5f437

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['6_201_200_206_20a_202016_2004_2012_0',['&lt;a href=&quot;https://github.com/nRF24/RF24Network/compare/v1.0.5...v1.0.6&quot; &gt;1.0.6&lt;/a&gt; - 2016-04-12',['../md_CHANGELOG.html#autotoc_md77',1,'']]],
-  ['6_20a_202016_2004_2012_1',['&lt;a href=&quot;https://github.com/nRF24/RF24Network/compare/v1.0.5...v1.0.6&quot; &gt;1.0.6&lt;/a&gt; - 2016-04-12',['../md_CHANGELOG.html#autotoc_md77',1,'']]],
-  ['6_20v1_200_207_201_200_207_20a_202016_2009_2020_2',['&lt;a href=&quot;https://github.com/nRF24/RF24Network/compare/v1.0.6...v1.0.7&quot; &gt;1.0.7&lt;/a&gt; - 2016-09-20',['../md_CHANGELOG.html#autotoc_md72',1,'']]]
+  ['6_201_200_206_20a_202016_2004_2012_0',['&lt;a href=&quot;https://github.com/nRF24/RF24Network/compare/v1.0.5...v1.0.6&quot; &gt;1.0.6&lt;/a&gt; - 2016-04-12',['../md_CHANGELOG.html#autotoc_md80',1,'']]],
+  ['6_20a_202016_2004_2012_1',['&lt;a href=&quot;https://github.com/nRF24/RF24Network/compare/v1.0.5...v1.0.6&quot; &gt;1.0.6&lt;/a&gt; - 2016-04-12',['../md_CHANGELOG.html#autotoc_md80',1,'']]],
+  ['6_20v1_200_207_201_200_207_20a_202016_2009_2020_2',['&lt;a href=&quot;https://github.com/nRF24/RF24Network/compare/v1.0.6...v1.0.7&quot; &gt;1.0.7&lt;/a&gt; - 2016-09-20',['../md_CHANGELOG.html#autotoc_md75',1,'']]]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -2,10 +2,10 @@ var searchData=
 [
   ['enable_5fdynamic_5fpayloads_0',['ENABLE_DYNAMIC_PAYLOADS',['../RF24Network__config_8h.html#ad899459e243820796dd98c2afb044218',1,'RF24Network_config.h']]],
   ['esbnetwork_1',['ESBNetwork',['../classESBNetwork.html',1,'ESBNetwork&lt; radio_t &gt;'],['../classESBNetwork.html#a0ac71a8989fe3d7cdc86b1bb538a8251',1,'ESBNetwork::ESBNetwork()']]],
-  ['example_201_2',['Example 1',['../md_docs_2tuning.html#autotoc_md139',1,'']]],
-  ['example_202_3',['Example 2',['../md_docs_2tuning.html#autotoc_md140',1,'']]],
-  ['expressing_20rf24network_20addresses_20in_20ip_20format_4',['Expressing RF24Network addresses in IP format',['../md_docs_2tuning.html#autotoc_md131',1,'']]],
-  ['extended_20timeouts_5',['Auto-Retry Count and Extended Timeouts',['../md_docs_2tuning.html#autotoc_md136',1,'']]],
+  ['example_201_2',['Example 1',['../md_docs_2tuning.html#autotoc_md142',1,'']]],
+  ['example_202_3',['Example 2',['../md_docs_2tuning.html#autotoc_md143',1,'']]],
+  ['expressing_20rf24network_20addresses_20in_20ip_20format_4',['Expressing RF24Network addresses in IP format',['../md_docs_2tuning.html#autotoc_md134',1,'']]],
+  ['extended_20timeouts_5',['Auto-Retry Count and Extended Timeouts',['../md_docs_2tuning.html#autotoc_md139',1,'']]],
   ['external_5fdata_5ftype_6',['EXTERNAL_DATA_TYPE',['../group__DEFINED__TYPES.html#gac6bbd2772e776d10ca7c5f3c7eddf982',1,'RF24Network.h']]],
   ['external_5fqueue_7',['external_queue',['../classESBNetwork.html#a1af1d7e26417c772adb897122d4368f1',1,'ESBNetwork']]]
 ];
