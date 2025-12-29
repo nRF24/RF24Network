a4cf1df16e6f9948e3466b11527eb42f4e7e9982
TMRh20 <tmrh20@gmail.com>
2025-12-29 08:14:05 -0600

    Update RF24Network.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- RF24Network.h
+++ RF24Network.h
@@ -970,7 +970,7 @@ private:
     uint8_t frame_queue[MAIN_BUFFER_SIZE]; /** Space for a small set of frames that need to be delivered to the app layer */
     #endif
 
-    uint8_t* next_frame; /** Pointer into the @p frame_queue where we should place the next received frame */
+    uint8_t* next_frame;                                 /** Pointer into the @p frame_queue where we should place the next received frame */
 
     #if !defined(DISABLE_FRAGMENTATION)
     RF24NetworkFrame frag_queue;                         /* a cache for re-assembling incoming message fragments */
