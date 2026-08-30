93f611ca1452c780a57e972b254dd68ab51654a6
TMRh20 <tmrh20@gmail.com>
2026-08-30 05:30:59 -0600

    Add Zephyr support (#276)
    - Fix for Zephyr Project based boards

--- RF24Network_config.h
+++ RF24Network_config.h
@@ -61,7 +61,7 @@
      * the actual transmitted payload to 24 bytes (which is also the default behavior on ATTiny devices).
      */
     #ifndef MAX_PAYLOAD_SIZE
-        #if defined linux || defined __linux || !defined F_CPU || F_CPU >= 45000000
+        #if defined linux || defined __linux || !defined F_CPU || F_CPU >= 45000000 || defined __ZEPHYR__
             #define MAX_PAYLOAD_SIZE 1514
         #else
             #define MAX_PAYLOAD_SIZE 144
